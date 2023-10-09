99730125dbb83e2def4d478c9605a2b8b91fc80d
BorisDmitriev <113904873+BorisDmitriev@users.noreply.github.com>
2023-10-09 20:47:54 +0200

    Initial commit

---
+++ solution.js
@@ -0,0 +1,46 @@
+let snackServings = 10;
+let guests = 20;
+
+// 1
+console.log(snackServings+guests);
+
+// 2
+console.log( snackServings - guests );
+console.log( guests - snackServings );
+
+//3
+console.log( guests * snackServings );
+
+//4
+console.log( snackServings / guests );
+
+//5
+let drinks = 10;
+let resultOne = snackServings*guests/drinks;
+console.log(resultOne);
+
+//6
+let monkeys = 15;
+let bananas = 9;
+console.log(monkeys / bananas);
+
+//7
+let monkeyfights = 20;
+let resultTwo = (monkeys+bananas)*monkeyfights;
+console.log( resultTwo );
+
+//8
+let monkeyBusiness = 90;
+monkeyBusiness++;
+
+//9
+let boredom = 13;
+boredom--;
+
+//10
+let happiness = monkeys-bananas;
+console.log(happiness + monkeyfights);
+
+//11
+console.log(resultOne%resultTwo);
+
